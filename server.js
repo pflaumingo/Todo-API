bd5c56cc41951ec2e5c747878137e493c013fd4f
Noah Pflaum <pflauming@gmail.com>
2016-05-11 14:13:59 -0700

    Add todo delete API method

--- server.js
+++ server.js
@@ -48,15 +48,15 @@ app.post('/todos', function (req, res) {
 });
 
 app.delete('/todos/:id', function (req, res) {
-    var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-    console.log(todoId);
-    if (!matchedTodo) {
-        res.status(404).json({"error": "No todo found with provided id"});
-    } else {
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
-    }
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(PORT, function () {

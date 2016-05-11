67d7098a508abda34fcd2366af07a31985942b28
Noah Pflaum <pflauming@gmail.com>
2016-05-11 13:34:57 -0700

    Refactor API using underscore

--- server.js
+++ server.js
@@ -32,16 +32,16 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
+    
+    body.description = body.description.trim();
 
-	// add id field
 	body.id = todoNextId++;
 
-	// push body into array
 	todos.push(body);
 	
 	res.json(body);

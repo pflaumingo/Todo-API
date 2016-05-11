50c50c09a16fbea3ca6f56ca0842e7414afb9123
Noah Pflaum <pflauming@gmail.com>
2016-05-11 16:42:49 -0700

    Final static todos

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.get('/todos', function (req, res) {
         filteredTodos = _.where(todos, {completed: false});
     }
     
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, function (todo) {
+            return todo.description.indexOf(queryParams.q) >-1;
+        });
+    }
+    
     res.json(filteredTodos);
 });
 

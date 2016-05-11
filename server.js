f0dd28daec598065e7eb603a2d552c1e18fd7233
Noah Pflaum <pflauming@gmail.com>
2016-05-11 10:17:54 -0700

    update zip

--- server.js
+++ server.js
@@ -52,40 +52,37 @@ app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
 
-    res.send('Todo API root');
-});
-
-app.get('/todos', function (req, res) {
-    res.json(todos);
-});
-
-app.get('/todos/:id', function (req, res) {
-    var todoId = parseInt(req.params.id);
-    var matchedTodo;
-    
-    todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
-    
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
-});
-
-app.post('/todos', function (req, res) {
-    var body = req.body;
-    body.id = todoNextId;
-    todoNextId++;
-    todos.push(body);
-    
-    console.log('Description: ' + body.description);
-    res.json(body);
-});
-
-app.listen(PORT, function() {
-    console.log('Express listening on port ' + PORT + '!');
-});
+//app.get('/todos', function (req, res) {
+//    res.json(todos);
+//});
+//
+//app.get('/todos/:id', function (req, res) {
+//    var todoId = parseInt(req.params.id);
+//    var matchedTodo;
+//    
+//    todos.forEach(function (todo) {
+//        if (todoId === todo.id) {
+//            matchedTodo = todo;
+//        }
+//    });
+//    
+//    if (matchedTodo) {
+//        res.json(matchedTodo);
+//    } else {
+//        res.status(404).send();
+//    }
+//});
+//
+//app.post('/todos', function (req, res) {
+//    var body = req.body;
+//    body.id = todoNextId;
+//    todoNextId++;
+//    todos.push(body);
+//    
+//    console.log('Description: ' + body.description);
+//    res.json(body);
+//});
+//
+//app.listen(PORT, function() {
+//    console.log('Express listening on port ' + PORT + '!');
+//});

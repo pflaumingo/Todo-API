7ce445d8162d7e9d7caf97836651b0dd29cad506
Noah Pflaum <pflauming@gmail.com>
2016-05-11 10:00:36 -0700

    Update to zip download

--- server.js
+++ server.js
@@ -1,45 +1,71 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Finish API',
-    completed: false,
-}, {
-    id: 2,
-    description: 'Go to work',
-    completed: false
-}, {
-    id: 3,
-    description: 'Go for a run',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send('Todo API Root');
+	res.send('Todo API Root');
 });
 
+// GET /todos
 app.get('/todos', function (req, res) {
-    res.json(todos);
+	res.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
-    var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    
-    todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 });
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	// add id field
+	body.id = todoNextId++;
+
+	// push body into array
+	todos.push(body);
+	
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT + '!');
+	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

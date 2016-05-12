8a6af544e42483b1ee54a61ed81df98e237ea28e
Noah Pflaum <pflauming@gmail.com>
2016-05-11 18:02:08 -0700

    Sequelize test

---
+++ playground/variable-playground.js
@@ -0,0 +1,26 @@
+//var person = {
+//    name: 'Andrew',
+//    age: 21
+//};
+//
+//function updatePerson (obj) {
+////    obj = {
+////        name: 'Noah',
+////        age: 25
+////    };
+//    
+//    obj.age = 25;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+var grades = [67, 80];
+
+var app = function addGrades (grades) {
+    grades.push(55);
+};
+
+app(grades);
+//addGrades(grades);
+console.log(grades);
